7a590b99cfd86130938859ec5cf5f1968c8caa80
ohhsow <pari@tut.by>
2017-10-14 10:19:28 +0300

    update

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -4,7 +4,14 @@ set -e
 
 count=$(find . -type d -name "node_modules" | wc -l)
 
+<<<<<<< 715e64a66d738b67d147cfd2087f234b031edbef
 if [ "$count" -eq "0" ]; then
+=======
+if [ -d "$DIR" ]; then
+
+	gulp dev
+else
+>>>>>>> update
 	npm install
     gulp dev
 else
