00bfc4b12dacf33fe97e60934e4cf7262da0c64c
Alex P <alexfile123@gmail.com>
2017-11-04 15:02:40 +0300

    update

--- nginx/ssl/check.sh
+++ nginx/ssl/check.sh
@@ -3,7 +3,7 @@
 set -ex
 
 count1=$(find /etc/nginx/ssl/* -type d -name "intodevops.by" | wc -l)
-count2=$(find /etc/nginx/conf.d/* -type f -name "ssl-intodevops" | wc -l)
+count2=$(find /etc/nginx/conf.d/* -type f -name "ssl-intodevops$" | wc -l)
 
 if [ "$count1" -gt "0" ] && [ "$count2" -gt "0" ]; then
 	mv /etc/nginx/conf.d/ssl-intodevops /etc/nginx/conf.d/ssl-intodevops.conf
