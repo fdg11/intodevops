e8ce5eacf51d92850cda0d750ffce27daceaef15
Alex P <alexfile123@gmail.com>
2017-11-04 14:26:15 +0300

    update

--- nginx/ssl/check.sh
+++ nginx/ssl/check.sh
@@ -7,4 +7,7 @@ count=$(find /etc/nginx/ssl/* -type d -name "intodevops.by" | wc -l)
 if [ "$count" -gt "0" ]; then
 	mv /etc/nginx/conf.d/ssl-intodevops /etc/nginx/conf.d/ssl-intodevops.conf
     sed -i 's/#return/return/g' /etc/nginx/conf.d/intodevops.conf
+    nginx -g "daemon off;"
+else
+    nginx -g "daemon off;"
 fi
