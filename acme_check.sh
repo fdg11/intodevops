3d831a8516ea03222a75b32b36768e5c5b34daea
Alex P <alexfile123@gmail.com>
2017-11-07 20:14:16 +0300

    update

--- acme_check.sh
+++ acme_check.sh
@@ -70,5 +70,5 @@ EOF
     sed -i 's/#return/return/g' nginx/conf.d/intodevops.conf
     docker-compose -p prod exec proxy nginx -s reload
 else
-    docker-compose -p prod exec proxy nginx restart
+    docker-compose -p prod exec proxy nginx -s reload
 fi
